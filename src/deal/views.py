c8ce5949ff1e37ee9726894f1159be1730738388
ldhuy <bongmaxuyentuong1@gmail.com>
2025-03-24 14:12:00 +0700

    Get list stage with permission

--- src/deal/views.py
+++ src/deal/views.py
@@ -1,9 +1,11 @@
-from rest_framework import status, viewsets
+from drf_yasg.utils import swagger_auto_schema
+from rest_framework import status
+from rest_framework.decorators import action
 from rest_framework.response import Response
 
 from common.views import SortAndFilterViewSet
-from deal.models import Deal
-from deal.serializers import DealDetailSerializer, DealSerializer
+from deal.models import Deal, Stage
+from deal.serializers import DealDetailSerializer, DealSerializer, StageDetailSerializer
 from utilities.permissions.custom_permissions import CustomPermission
 
 # Create your views here.
@@ -19,6 +21,8 @@ class DealViewSet(SortAndFilterViewSet):
             return DealDetailSerializer
         if self.action in ["create", "update"]:
             return DealSerializer
+        if self.action == "get_list_stage":
+            return StageDetailSerializer
 
     def get_queryset(self):
         return Deal.objects.all()
@@ -65,3 +69,26 @@ class DealViewSet(SortAndFilterViewSet):
         )
 
         return Response(DealDetailSerializer(data).data, status=status.HTTP_200_OK)
+
+    @swagger_auto_schema()
+    @action(
+        detail=False,
+        methods=["get"],
+        url_path="stage",
+    )
+    def get_list_stage(self, request):
+        queryset = Stage.objects.all()
+        if not getattr(request, "is_view_all", None):
+            queryset = queryset.exclude(probability__in=[0.0, 1.0])
+
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            serialize = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serialize.data)
+        return Response(
+            self.get_serializer(
+                queryset,
+                many=True,
+            ).data,
+            status=status.HTTP_200_OK,
+        )
