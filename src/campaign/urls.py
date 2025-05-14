7cd102f2eb1fa2b42679d34a00eae56d2e7a1da6
ldhuy <bongmaxuyentuong1@gmail.com>
2025-05-14 23:04:34 +0700

    update api list campaign

--- src/campaign/urls.py
+++ src/campaign/urls.py
@@ -1,13 +1,13 @@
 from django.urls import include, path, re_path
 from rest_framework.routers import SimpleRouter
 
-from campaign.views import CampaignViewSet
+from campaign.views import CampaignViewSet, CampaignStatusAPIView, CampaignTypeAPIView
 
 router = SimpleRouter()
 router.register(r"", CampaignViewSet, "campaigns")
 
 urlpatterns = [
-    # path("status/", CampaignStatusAPIView.as_view(), name="campaign_status"),
-    # path("types/", CampaignTypeAPIView.as_view(), name="campaign_types"),
+    path("statuses/", CampaignStatusAPIView.as_view(), name="campaign_status"),
+    path("types/", CampaignTypeAPIView.as_view(), name="campaign_types"),
     re_path(r"^", include(router.urls)),
 ]
